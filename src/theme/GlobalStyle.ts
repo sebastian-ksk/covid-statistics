403a09bdaccaa894d283dc481325e4039a6889f3
sebastian-ksk <castellanospsebastian@gmail.com>
2022-11-23 00:22:17 -0500

    complete general data card

--- src/theme/GlobalStyle.ts
+++ src/theme/GlobalStyle.ts
@@ -3,18 +3,15 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   html {
     margin: 0px;
-    
     font-family: 'Roboto Condensed', sans-serif;
   }
   *{
-    padding: 0;
-    margin: 0;
-
+    padding: 0px;
+    margin: 0px;
     font-family: 'Roboto Condensed', sans-serif;
   }
   :root{
     margin: 0px;
-   
     font-family: 'Roboto Condensed', sans-serif;
   }
 `;
